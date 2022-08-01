bd570710f15a695fd14ebf50b624ab85f90e2dfa
Max <maksimhomisak64@gmail.com>
2022-08-01 13:27:48 +0300

    Applied Transaction technology

--- src/main/java/com/maxkhomoshchak/MovieApi/controller/ViewController.java
+++ src/main/java/com/maxkhomoshchak/MovieApi/controller/ViewController.java
@@ -1,69 +1,70 @@
 package com.maxkhomoshchak.MovieApi.controller;
 
 import com.maxkhomoshchak.MovieApi.domain.Movie;
+import com.maxkhomoshchak.MovieApi.domain.Transaction;
 import com.maxkhomoshchak.MovieApi.domain.User;
 import com.maxkhomoshchak.MovieApi.service.MovieService;
+import com.maxkhomoshchak.MovieApi.service.TransactionService;
 import com.maxkhomoshchak.MovieApi.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ViewController {
 
-    private MovieService movieService;
+//    private MovieService movieService;
     private UserService userService;
+    private TransactionService transactionService;
 
     @Autowired
-    public ViewController(MovieService movieService, UserService userService) {
+    public ViewController(/*MovieService movieService,*/ UserService userService, TransactionService transactionService) {
 
-        this.movieService = movieService;
+//        this.movieService = movieService;
         this.userService = userService;
+        this.transactionService = transactionService;
     }
 
     @GetMapping("/")
-    public String mainView(){
+    public String mainView(Model model){
         return "Main";
     }
 
-    @GetMapping("/Success")
-    public String success(){
-        return "Success";
+    @GetMapping("/register")
+    public String registerView(Model model){
+        return "registration_form";
     }
 
     @PostMapping
-    public String submit(Movie movie){
+    public String submit(@AuthenticationPrincipal User user, Movie movie, RedirectAttributes redirectAttributes){
 
-        movieService.submitMovie(movie);
+        String s = transactionService.submitTransaction(user, movie);
 
-        return "redirect:/Success";
-    }
-
-    @PostMapping("/createUser")
-    public String createUser(User user){
-
-        userService.create(user);
-
-        return "redirect:/Success";
-    }
-
-    @PostMapping("/toHomePage")
-    public String toHomePage(){
+        redirectAttributes.addFlashAttribute("result", s);
 
         return "redirect:/";
     }
 
+    @PostMapping("/createUser")
+    public String createUser(User user, RedirectAttributes redirectAttributes){
 
-    @PostMapping("/getMovies")
-    public String viewMovies(){
+         String s = userService.create(user);
 
-        return "redirect:/MoviesApi";
-    }
+         if(s.equals("")){
 
-    @PostMapping("/logIn")
-    public String login(){
+             return "redirect:/login";
+         }else{
 
-        return "redirect:/login";
+             redirectAttributes.addFlashAttribute("message", s);
+
+             return "redirect:/register";
+         }
     }
 }
